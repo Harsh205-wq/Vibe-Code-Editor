5decaf91942206bb492f649b06b77d202e0c66cf
Harsh <harshupadhayay23118711xyz@gmail.com>
2026-02-28 23:12:24 +0530

    Signup error

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 import { db } from "@/lib/db"
+import UserButton from "@/modules/auth/componets/user.button";
 
 export default async function Home() {
-  const users = await db.user.findMany()
-  console.log(users)
-
-  return <div>Check terminal</div>
+<div>
+  <Button> Get Started</Button>
+  <UserButton></UserButton>
+  </div>
 }
